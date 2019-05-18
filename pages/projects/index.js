56d191fd9c37dfc1713fcf98f688cfe590ab23aa
Andr3wRiv3rs <andrewarivers@gmail.com>
2019-05-17 18:59:32 -0700

    Finished Projects page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,6 +2,82 @@ import Link from 'next/link'
 import css from './index.sass'
 import Projects from '../../projects.js'
 
+const Screenshots = (props) => (
+    props.project.screenshots ? props.project.screenshots.map( screenshot => 
+        <div 
+            key={ 'screenshot-' + screenshot + props.keyAddition || '' } 
+            className={ css.screenshot } 
+            style={{
+                backgroundImage: `
+                    url(/static/projects/${props.project.id}/screenshots/${screenshot}.png)
+                `
+            }} 
+        />
+    ) : ''
+)   
+
+const Links = (props) => (
+    props.project.links ? props.project.links.map( link => 
+        <a 
+            key={ link.url } 
+            type={ link.type } 
+            target="_blank" 
+            className={ css.project_link } 
+            href={ link.url } 
+            style={{
+                backgroundImage: `
+                    url(/static/icons/${link.type}_circle.svg)
+                `
+            }} 
+        />
+    ) : ''
+)
+
+const Handles = (props) => (
+    props.project.handles ? props.project.handles.map( handle => 
+        <a
+            key={ handle.value }
+            type={ handle.type }
+            target="_blank"
+            className={ `${css.handle} ${css.link}` }
+            href={ {
+                twitter: "https://twitter.com/@",
+                instagram: "https://www.instagram.com/",
+                gamejolt: "https://gamejolt.com/@",
+            }[handle.type] + handle.value }
+        >
+            @{ handle.value }
+        </a>
+    ) : ''
+)
+
+const Description = (props) => {
+
+    let { description } = props.project
+
+    description = description.trim().split(/(?!\[[^\[\]]+)\s(?![^\[\]]+\])/g)
+
+    for (let i=0; i<description.length; i++) {
+        description[i] = ` ${description[i]} `
+
+        const link = /(\[[^\[\]]+\])(\([^\(\)]+\))/g.exec(description[i])
+
+        if (link !== null)
+            description[i] = 
+                <a 
+                    key={ link[2] } 
+                    className={ css.link }
+                    target="_blank"
+                    href={ link[2].replace(/(\(|\))/g, '') }>{ 
+                    link[1].replace(/(\[|\])/g, '') }
+                </a>
+    }
+    
+    return (
+        <p key={ description } className={ css.description }>{ description }</p>
+    )
+}
+
 export default class extends React.Component {
     componentDidMount () {
         const elements = document.querySelectorAll(`.${css.screenshots}`)
@@ -9,91 +85,56 @@ export default class extends React.Component {
         for (const screenshots of elements) 
             screenshots.scrollInterval = setInterval( 
                 () => this.scrollScreenshots(screenshots),
-                16.66
+                16.6
             )
     }
 
     scrollScreenshots (screenshots) {
-        let { scrollTop, scrollHeight, offsetHeight } = screenshots
+        let { style: { top }, clientHeight } = screenshots.children[0]
 
-        scrollTop++
+        top = parseInt(top || 0) - 1
 
-        if ( scrollTop === (scrollHeight - offsetHeight)/2 + offsetHeight/2 ) 
-            scrollTop = 0
+        if ( top * -1 >= clientHeight/2 ) top = 0
 
-        screenshots.scrollTop = scrollTop
-    }
-
-    getProjects () {
-        const projects = []
-
-        for (const project of Projects) {
-            const screenshots = [], links = []
-
-            for (const screenshot of project.screenshots)
-                screenshots.push(
-                    <div key={ screenshot } className={ css.screenshot } style={{
-                        backgroundImage: `url(/static/projects/${project.id}/screenshots/${screenshot}.png)`
-                    }} />
-                )
-
-            for (const screenshot of project.screenshots)
-                screenshots.push(
-                    <div key={ screenshot + '0' } className={ css.screenshot } style={{
-                        backgroundImage: `url(/static/projects/${project.id}/screenshots/${screenshot}.png)`
-                    }} />
-                )
-
-
-            for (const link of project.links)
-                links.push(
-                    <a 
-                        key={ link.url } 
-                        type={ link.type } 
-                        target="_blank" 
-                        className={ css.link } 
-                        href={ link.url } 
-                        style={{
-                            backgroundImage: `url(/static/icons/${link.type}_circle.svg)`
-                        }} 
-                    />
-                )
-
-            projects.push(
-                <div 
-                    key={ project.id } 
-                    className={ `${css.project}` } 
-                    style={{
-                        backgroundImage: `url(/static/projects/${project.id}/${project.id}.png)`
-                    }}
-                >
-                    <div>
-                        <div className={ css.info }>
-                            <div>
-                                <h2 className={ css.title }>{ project.title }</h2>
-                                { links }
-                            </div>
-
-                            <p className={ css.description }>{ project.description }</p>
-                        </div>
-
-                        <div className={ css.screenshots }>
-                            <div>
-                                { screenshots }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
-
-        return projects
+        screenshots.children[0].style.top = top + 'px'
     }
 
     render () {
         return (
             <div className={ css.projects }>
-                { this.getProjects() }
+                {
+                    Projects.map( project =>
+                        <div 
+                            key={ project.id } 
+                            className={ `${css.project}` } 
+                            style={{
+                                backgroundImage: `url(/static/projects/${project.id}/${project.id}.png)`
+                            }}
+                        >
+                            <div>
+                                <div className={ css.info }>
+                                    <div>
+                                        <h2 className={ css.title }>{ project.title }</h2>
+                                        { <Links project={ project } />}
+                                    </div>
+
+                                    <div>
+                                        { <Handles project={ project } /> }
+                                    </div>
+
+                                    <Description project={ project } description={ project.description }/>
+                                </div>
+
+                                <div className={ css.screenshots }>
+                                    <div>
+                                        { <Screenshots project={ project } /> } 
+                                        { <Screenshots project={ project } keyAddition={ `0` } /> } 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    )
+                }
             </div>
         )
     }
--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ export default [
         id: "corgi_stack_3d",
 
         description: `
-            Sequel to my entry for the 2016 Corgsmas game jam, Corgi Stack 3D is a 
+            Sequel to [my entry](https://jams.gamejolt.io/corgsmas/games/corgi-stack/218872) for the [Corgsmas](https://jams.gamejolt.io/corgsmas/games) game jam, Corgi Stack 3D is a 
             reimagined puzzle-platformer which uses cute dogs as moving platforms.
         `,
 
@@ -15,7 +15,7 @@ export default [
         handles: [
             {
                 type: "twitter",
-                value: "@Andr3wRiv3rs"
+                value: "Andr3wRiv3rs"
             },
 
             {
@@ -34,6 +34,277 @@ export default [
                 type: "github",
                 url: "https://github.com/Andr3wRiv3rs/Corgi-Stack-3D"
             },
+
+            {
+                type: "discord",
+                url: "https://discord.gg/taqbtNn"
+            }
         ]
     },
+    
+    {
+        title: "Kismet",
+        id: "kismet",
+
+        description: "An unannounced surreal horror project.",
+
+        screenshots: [
+            1, 2, 3, 4, 5
+        ],
+
+        handles: [
+            {
+                type: "twitter",
+                value: "Andr3wRiv3rs"
+            }, 
+            {
+                type: "twitter",
+                value: "PinkMayoMusic"
+            }, 
+            {
+                type: "twitter",
+                value: "AnonMaGames"
+            },  
+            {
+                type: "gamejolt",
+                value: "anthonyme00"
+            }, 
+        ],
+        
+        links: []
+    }, 
+    
+    {
+        title: "YaaiiA",
+        id: "yaaiia",
+
+        description: `
+            Short for "Yet another alien invasion in Atlanta", 
+            this was an entry for the [Dreamhack Atlanta 2018](https://jams.gamejolt.io/dhjam) game jam, developed in Unity.
+        `,
+
+        screenshots: [
+            1, 2, 3, 4, 5
+        ],
+        
+        handles: [
+            {
+                type: "twitter",
+                value: "Andr3wRiv3rs"
+
+            }, 
+
+            {
+                type: "gamejolt",
+                value: "anthonyme00"
+            }, 
+
+            {
+                type: "twitter",
+                value: "twpunkster"
+            },  
+        ],
+
+        links: [
+            {
+                type: "gamejolt", 
+                url: "https://gamejolt.com/games/yaaiia/377383"
+            }
+        ]
+    },
+
+    {
+        title: "Devcord Challenges",
+        id: "devcord_challenges",
+
+        description: `
+            A community driven coding challenge app, 
+            built in Nuxt.js by myself for the Devcord community.
+        `,
+
+        links: [
+            {
+                type: "devcord",
+                url: "https://challenge.devcord.com"
+            },
+
+            {
+                type: "discord",
+                url: "https://discord.gg/EhTaURr"
+            }
+        ],
+    },
+    
+    {
+        title: "Mere Mercy",
+
+        id: "mere_mercy",
+        description: "Mere Mercy is a Zombie Survival game for Android, built in Unity.",
+
+        screenshots: [],
+
+        handles: [
+            {
+                type: "twitter",
+                value: "TheAtheistSheep"
+
+            }, 
+
+            {
+                type: "twitter",
+                value: "Andr3wRiv3rs"
+
+            }, 
+
+            {
+                type: "twitter",
+                value: "DablTrabl2"
+
+            }, 
+
+            {
+                type: "twitter",
+                value: "MikeyBoyii"
+
+            }, 
+
+            {
+                type: "instagram",
+                value: "evanrossney"
+
+            },
+        ],
+
+        links: []
+    }, 
+
+    {
+        title: "Malus",
+        id: "malus",
+
+        description: `
+            A UPnP portmapper, built using [my wrapper](https://www.npmjs.com/package/nat-upnp-wrapper) 
+            for the [nat-upnp](https://www.npmjs.com/package/nat-upnp) module.
+        `,
+
+        links: [
+            {
+                type: "github",
+                url: "https://github.com/Andr3wRiv3rs/port-mapper"
+            }
+        ]
+    },
+    
+    {
+        title: "Twilight Punkster Galaxy",
+        id: "twilightpunkster",
+
+        description: `
+            Worked on the MMORPG "Twilight Punkster Galaxy" 
+            as a fullstack web developer, using Express and Vue.js.
+        `,
+
+        screenshots: [],
+
+        links: [
+            {
+                type: "medium", 
+                url: "https://medium.com/@twpunkster/twilight-punkster-651cf3a52954"
+            }, 
+            
+            {
+                type: "twitter",
+                url: "https://twitter.com/twpunkster"
+            }
+        ]
+    }, 
+    
+    {
+        title: "Stardust",
+        id: "stardust",
+
+        description: "Briefly worked with [Stardust](https://www.stardust.gg/)'s blockchain technologies, building a client in Electron.",
+
+        screenshots: [],
+        links: [],
+
+        left: true
+    },  
+    
+    {
+        title: "Cherry",
+        id: "cherry",
+
+        description: "My chatbot built in Node.js with Discord.js. Cherry is capable of playing music, giving definitions, and automating color roles among other things.",
+
+        screenshots: [],
+
+        links: [
+            {
+                type: "discord",
+                url: "https://discordapp.com/api/oauth2/authorize?client_id=443491966576230400&permissions=8&scope=bot"
+            }, {
+                type: "github",
+                url: "https://github.com/Andr3wRiv3rs/Cherry"
+            }
+        ],
+
+        left: true
+    }, 
+    
+    {
+        title: "Untitled Worm I/O",
+
+        id: "worm_io",
+
+        description: "A Slither.io inspired project, utilizing cryptocurrency as a driving mechanic.",
+
+        screenshots: [],
+
+        links: [
+            {
+                type: "github",
+                url: "https://github.com/Andr3wRiv3rs/worm-io"
+            }
+        ]
+    }, 
+    
+    {
+        title: "Shh",
+        id: "shh",
+
+        description: "A Peer to peer encrypted chat client built in Electron with WebRTC and WebSockets.",
+
+        screenshots: [],
+
+        links: [
+            {
+                type: "github",
+                url: "https://github.com/Andr3wRiv3rs/Shh-Client"
+            }
+        ],
+
+        left: true
+    }, 
+    
+    {
+        title: "Bearmon",
+        id: "bearmon",
+
+        description: "An online multiplayer turn based RPG for the 2017 Bear Week jam on Game Jolt.",
+
+        screenshots: [],
+
+        links: [
+            {
+                type: "gamejolt",
+                url: "https://gamejolt.com/games/bearmon/276536"
+            }, {
+                type: "github",
+                url: "https://github.com/Andr3wRiv3rs/bearmon-2017"
+            }
+        ],
+
+        left: true
+    }
 ]
